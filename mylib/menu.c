8ffcf8acbafb9b53752036f41dcee63bd22b7089
Lian <gj39981225@163.com>
2025-10-04 22:24:33 +0800

    feat(menu): initialization

---
+++ mylib/menu.c
@@ -0,0 +1,11 @@
+#include "menu.h"
+
+void menu() {
+    printf("\n===== 朋友管理系统 =====\n");
+    printf("1. 添加朋友\n");
+    printf("2. 删除朋友\n");
+    printf("3. 查询朋友\n");
+    printf("4. 所有朋友\n");
+    printf("5. 退出...\n");
+    printf("请选择操作: ");
+}
