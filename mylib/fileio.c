44a1a493f604e5965974a31234b1e1a3ca91ac66
Lian <gj39981225@163.com>
2025-10-04 22:22:40 +0800

    feat(fileio): add solutions for windows and linux

--- mylib/fileio.c
+++ mylib/fileio.c
@@ -2,7 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <direct.h>
+#include <sys/types.h>
+
+#ifdef _WIN32
+    #include <direct.h>
+#endif
 
 #define MAX_LINE_LENGTH 256
 
@@ -12,12 +16,20 @@ int load_friends_from_csv(const char *filename, Friend *friends, int *friend_cou
 
     struct stat st = {0};
     if (stat("data", &st) == -1) {
+#ifdef _WIN32
         if (_mkdir("data") != 0) {
             perror("无法创建 data 目录");
             return 0;
         }
+#else
+        if (mkdir("data", 0755) != 0) {
+            perror("无法创建 data 目录");
+            return 0;
+        }
+#endif
     }
 
+    // 打开文件，如果不存在则新建
     FILE *file = fopen(filepath, "r");
     if (!file) {
         file = fopen(filepath, "w");
@@ -33,15 +45,15 @@ int load_friends_from_csv(const char *filename, Friend *friends, int *friend_cou
     *friend_count = 0;
 
     while (fgets(line, sizeof(line), file)) {
-    if (sscanf(line, "%d,%49[^,],%d,%9[^,],%199[^\n]",
-                &friends[*friend_count].id, 
-                friends[*friend_count].name, 
-                &friends[*friend_count].qqid, 
-                friends[*friend_count].gender, 
-                friends[*friend_count].desc) == 5) {
-        (*friend_count)++;
+        if (sscanf(line, "%d,%49[^,],%d,%9[^,],%199[^\n]",
+                    &friends[*friend_count].id, 
+                    friends[*friend_count].name, 
+                    &friends[*friend_count].qqid, 
+                    friends[*friend_count].gender, 
+                    friends[*friend_count].desc) == 5) {
+            (*friend_count)++;
+        }
     }
-}
 
     fclose(file);
     return 1;
@@ -69,3 +81,54 @@ int save_friends_to_csv(const char *filename, const Friend *friends, int friend_
     fclose(file);
     return 1;
 }
+
+/*
+示例调用
+#include <stdio.h>
+#include "fileio.h"
+
+#define MAX_FRIENDS 100
+
+int main() {
+    Friend friends[MAX_FRIENDS];
+    int friend_count = 0;
+
+    // 1. 从 CSV 加载
+    if (!load_friends_from_csv("friends.csv", friends, &friend_count)) {
+        printf("读取 CSV 失败！\n");
+        return 1;
+    }
+    printf("已加载 %d 个好友\n", friend_count);
+
+    // 2. 添加新好友
+    Friend new_friend = { 
+        .id = friend_count + 1,
+        .qqid = 12345678
+    };
+    snprintf(new_friend.name, sizeof(new_friend.name), "Alice");
+    snprintf(new_friend.gender, sizeof(new_friend.gender), "Female");
+    snprintf(new_friend.desc, sizeof(new_friend.desc), "喜欢编程的朋友");
+
+    friends[friend_count++] = new_friend;
+
+    // 3. 保存到 CSV
+    if (!save_friends_to_csv("friends.csv", friends, friend_count)) {
+        printf("保存 CSV 失败！\n");
+        return 1;
+    }
+
+    printf("已保存 %d 个好友\n", friend_count);
+
+    // 4. 打印所有好友
+    for (int i = 0; i < friend_count; i++) {
+        printf("ID=%d, Name=%s, QQ=%d, Gender=%s, Desc=%s\n",
+                friends[i].id,
+                friends[i].name,
+                friends[i].qqid,
+                friends[i].gender,
+                friends[i].desc);
+    }
+
+    return 0;
+}
+*/
--- mylib/fileio.h
+++ mylib/fileio.h
@@ -3,8 +3,10 @@
 
 #include "friend.h"
 
+// 加载朋友数据
 int load_friends_from_csv(const char *filename, Friend *friends, int *friend_count);
 
+// 保存朋友数据
 int save_friends_to_csv(const char *filename, const Friend *friends, int friend_count);
 
 #endif
