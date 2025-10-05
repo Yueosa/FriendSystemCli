28910113bbed9380700313533f13a2d0054846a8
Lian <gj39981225@163.com>
2025-10-05 21:57:25 +0800

    feat(system): ...
    - added new function "system_find_and_printf"
    - refactored output
    - corrected some function return values
    - modified audit descriptions

--- mylib/system.c
+++ mylib/system.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "system.h"
 #include "output.h"
 #include "audit.h"
@@ -11,44 +12,50 @@
 
 #define MAX_FRIEND 100
 
+
 enum SATA {
     SUCCESS = 0,
-    FIELD = 1,
+    FAILED = 1,
 };
+
 
 static Friend friends[MAX_FRIEND];
 static int friend_count = 0;
+
 
 static void system_cleanup() {
     friend_count = 0;
 }
 
+
 int system_init() {
     system_cleanup();
 
     if (load_friends_from_csv("friends.csv", friends, &friend_count)) {
-        output_error("无法加载");
-        return FIELD;
+        output_error("无法加载csv");
+        return FAILED;
     }
 
     if (audit_init("audit.log")) {
-        output_error("审计系统初始化失败");
-        return FIELD;
+        output_error("audit初始化失败");
+        return FAILED;
     }
     return SUCCESS;
 }
+
 
 int system_add_friend(Friend f) {
     if (friend_count >= MAX_FRIEND) {
         output_error("朋友太多了~塞不下了唔...");
         audit_log("ADD_ERROR", f.name);
-        return SUCCESS;
+        return FAILED;
     }
     friends[friend_count++] = f;
     output_info("太好了~是新的朋友...");
     audit_log("ADD", f.name);
     return SUCCESS;
 }
+
 
 int system_remove_friend(int id) {
     for (int i = 0; i < friend_count; i++) {
@@ -64,7 +71,7 @@ int system_remove_friend(int id) {
     char buf[32];
     sprintf(buf, "%d", id);
     audit_log("DELETE_ERROR", buf);
-    return FIELD;
+    return FAILED;
 }
 
 
@@ -83,15 +90,26 @@ Friend* system_find_friend(int id) {
     return NULL;
 }
 
+
+void system_find_and_printf(int id) {
+    Friend* f = system_find_friend(id);
+    if (f != NULL) {
+        print_friend(f);
+    } else {
+        output_warning("对象不存在");
+    }
+}
+
+
 int system_list_friends() {
     if (friend_count == 0) {
         output_warning("\n怎么一个朋友都没有...");
-        audit_log("LIST_ERROR", "0");
-        return FIELD;
+        audit_log("LIST_ERROR", "List is empty");
+        return FAILED;
     }
     printf("\n");
     for (int i = 0; i < friend_count; i++) {
-        printf("ID: %d | 名字: %s | QQ: %d | 性别: %s | 描述: %s\n",
+        printf("| ID: %d | 名字: %s | QQ: %d | 性别: %s | 描述: %s |\n",
                 friends[i].id,
                 friends[i].name,
                 friends[i].qqid,
@@ -99,14 +117,15 @@ int system_list_friends() {
                 friends[i].desc
                 );
     }
-    audit_log("LIST", "success");
+    audit_log("LIST", "Success");
     return SUCCESS;
 }
+
 
 int system_end() {
     if (save_friends_to_csv("friends.csv", friends, friend_count)) {
         output_error("无法保存数据");
-        return FIELD;
+        return FAILED;
     }
 
     system_cleanup();
--- mylib/system.h
+++ mylib/system.h
@@ -3,22 +3,26 @@
 
 #include "friend.h"
 
-// 初始化系统
+
 int system_init();
 
-// 添加朋友
+
 int system_add_friend(Friend f);
 
-// 删除朋友
+
 int system_remove_friend(int id);
 
-// 寻找朋友，返回指针
+
 Friend* system_find_friend(int id);
 
-// 列出所有朋友
+
+void system_find_and_printf(int id);
+
+
 int system_list_friends();
 
-// 清理
+
 int system_end();
+
 
 #endif
