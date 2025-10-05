625a068de32aae26a24f5edcb6e5b0d14811eb59
Lian <gj39981225@163.com>
2025-10-05 21:03:31 +0800

    chore(output): enter~

--- mylib/output.c
+++ mylib/output.c
@@ -1,14 +1,17 @@
-#include "output.h"
 #include <stdio.h>
+
+#include "output.h"
 
 
 void output_info(const char *message) {
     printf("[INFO] %s\n", message);
 }
 
+
 void output_error(const char *message) {
     printf("[ERROR] %s\n", message);
 }
+
 
 void output_warning(const char *message) {
     printf("[WARNING] %s\n", message);
